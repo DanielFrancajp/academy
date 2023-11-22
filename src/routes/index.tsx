f775c5b20c86ef6afbf542599f8c42392295d139
DanielFrancajp <danieldenotas@gmail.com>
2023-11-22 07:58:52 -0300

    finaly app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,20 @@
-import { Box, useTheme } from 'native-base';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { useTheme, Box } from 'native-base';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-import { AuthRoutes } from './auth.routes'
+import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
-    const { colors } = useTheme();
+  const { colors } = useTheme();
 
-    const theme = DefaultTheme;
-    theme.colors.background = colors.gray[700]
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[700];
 
-
-    return (
-
-        <Box flex={1} bg="gray.700">
-            <NavigationContainer theme={theme}>
-                <AuthRoutes />
-            </NavigationContainer>
-        </Box>
-    )
+  return (
+    <Box flex={1} bg="gray.700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
+  );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,77 +1,72 @@
-import { useNavigation } from '@react-navigation/native'
-import { VStack, Center, Image, Text, Heading, ScrollView } from 'native-base';
+import { useNavigation } from "@react-navigation/native";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
-import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
 import LogoSvg from '@assets/logo.svg';
-import BackGroundImg from '@assets/background.png';
+import BackgroundImg from '@assets/background.png';
 
-import { Input } from '@components/Input';
-import { Button } from '@components/Button';
-
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
 
-    const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
-    function handleNewAccount() {
-        navigation.navigate('signUp')
-    }
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  }
 
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} px={10} pb={16}>
+        <Image 
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-    return (
-        <ScrollView
-            contentContainerStyle={{ flexGrow: 1 }}
-            showsVerticalScrollIndicator={false}
-        >
-            <VStack flex={1} px={10} >
-                <Image
-                    source={BackGroundImg}
-                    defaultSource={BackGroundImg}
-                    alt='Pessoas treinando'
-                    resizeMode='contain'
-                    position="absolute"
-                />
-                <Center my={24}>
-                    <LogoSvg />
+        <Center my={24}>
+          <LogoSvg />
 
-                    <Text color={'gray.100'} fontSize="sm">
-                        Treine sua mente e o seu corpo
-                    </Text>
-                </Center>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
 
-                <Center>
-                    <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-                        Acecsse sua conta
-                    </Heading>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse a conta
+          </Heading>
 
-                    <Input
-                        placeholder='E-mail'
-                        keyboardType='email-address'
-                        autoCapitalize='none'
-                    />
-                    <Input
-                        placeholder='Senha'
-                        secureTextEntry
-                    />
-                    <Button
-                        title='Acessar'
-                    />
+          <Input 
+            placeholder="E-mail" 
+            keyboardType="email-address"
+            autoCapitalize="none"
 
-                </Center>
+          />
+          <Input 
+            placeholder="Senha" 
+            secureTextEntry
+          />
 
-                <Center mt={24}>
-                    <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-                        Ainda não tem acesso?
-                    </Text>
+          <Button title="Acessar" />
+        </Center>
 
-                </Center>
-                <Button
-                    title='Criar conta'
-                    variant="outline"
-                    onPress={handleNewAccount}
-                />
-            </VStack>
-        </ScrollView>
-    )
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+
+          <Button 
+            title="Criar Conta" 
+            variant="outline"
+            onPress={handleNewAccount}
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
+  );
 }
